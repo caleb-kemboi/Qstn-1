dcc5770964ffee3f89c903525ebd69f90e46b809
Caleb Kemboi <kemboicaleb5@gmail.com>
2023-06-19 18:02:23 +0300

    Add files via upload

---
+++ Qstn 1/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//Stored Procedure: UpdateEmployeeSalary
+
+CREATE PROCEDURE UpdateEmployeeSalary
+(IN empID INT, IN newSalary INT, OUT oldSalary INT)
+BEGIN
+    //Retrieve the old salary value
+    SELECT salary INTO oldSalary FROM employees WHERE id = empID;
+    
+    //Update the employee's salary
+    UPDATE employees SET salary = newSalary WHERE id = empID;
+END
+
+
+
+[TestFixture]
+public class EmployeeDALTests
+{
+    private MySqlConnection connection;
+    private EmployeeDAL dal;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Setup code to create a connection to the test database
+        connection = new MySqlConnection("your_connection_string_here");
+        dal = new EmployeeDAL(connection);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Code to tear down and clean up the test database
+        connection.Close();
+    }
+
+    [Test]
+    public void TestUpdateEmployeeSalary()
+    {
+        // Arrange
+        int empID = 1;
+        int newSalary = 50000;
+        int oldSalary;
+
+        // Act
+        dal.UpdateEmployeeSalary(empID, newSalary, out oldSalary);
+
+        // Assert
+        Assert.AreEqual(40000, oldSalary); // Assuming the old salary is 40000
+        // 
+}
+Add additional assertions as needed
+    }
+
+
